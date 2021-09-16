142b0c970f8dff49a69ae0382c6df9836c5d05dd
Elli <elli.pirnes@gmail.com>
2021-09-16 15:03:54 +0300

    harjoitus3 tehty

--- exercise1/src/components/Header.js
+++ exercise1/src/components/Header.js
@@ -3,22 +3,22 @@ import styles from './Header.module.css'
 
 export default function Header() {
 
-    const tilaa = {color: 'black', backgroundColor: 'white'};
+    const tilaa = {color: 'black', backgroundColor: 'white', marginLeft: 200};
 
     return (
         <div className={styles.headerBackground}>
             <div className={styles.container}>
                 <div className={styles.brand}>HELSINGIN SANOMAT </div>
-                <div>Etusivu </div>
-                <div>Uutiset </div>
-                <div>Lehdet </div>
-                <div>Asiakaspalvelu </div>
-                <div style={tilaa}>Tilaa </div>
-                <div>Kirjaudu </div>
-                <div>Valikko </div>
-
+                <div className= {styles.tekstit}> Etusivu </div>
+                <div className= {styles.tekstit}> Uutiset </div>
+                <div className= {styles.tekstit}> Lehdet </div>
+                <div className= {styles.tekstit}> Asiakaspalvelu </div>
+                <div className= {styles.tekstit}><span style={tilaa}>Tilaa </span></div>
+                <div className= {styles.tekstit}> Kirjaudu </div>
+                <div className= {styles.tekstit}> Valikko </div>
+                </div>
             </div>
 
-        </div>
+        
     )
 }
--- exercise1/src/components/MainSection.js
+++ exercise1/src/components/MainSection.js
@@ -7,6 +7,7 @@ export default function MainSection() {
         <div>
         <div className={styles.container}>
             <div className={styles.otsikko}>Hallitus</div>
+            <div><img src={`/hallitus.jpg`} /> </div>
             <span className={styles.subtitle}>Budjettiriihi | </span>
             <span className={styles.title}>Marin: Veroista saavutettu "hyvin lähelle" yhteinen näkemys - Hallitus aloitti budjettineuvottelut</span>
             <div>Politiikka: 10.45 </div>
